1799ccc6ade24d4f2d289c10c8884f53996fd6a5
Suleimane Ducure <suleimane.ducure@aluno.ufop.edu.br>
2021-05-24 16:03:30 -0300

    tetse no CSILab

--- terrestrialNetwork.py
+++ terrestrialNetwork.py
@@ -2,7 +2,7 @@ from graphBuilder import aindaPensar, graphBuilder
 from igraph import *
 
 
-g = Graph.Read_GraphML('Other/terrestrial.GraphML')
+g = Graph.Read_GraphML('Other/fluvial.GraphML')
 lst = aindaPensar(g)
 # graphBuilder(g, lst[1], lst[0], 'Fluvial', 'Terrestrial')
 
