d83719bb5cd88afc78c2a048320cbbb9b39ee727
Suleimane Ducure <suleimane.ducure@aluno.ufop.edu.br>
2021-05-24 21:56:58 -0300

    terrestrial

--- terrestrialNetwork.py
+++ terrestrialNetwork.py
@@ -6,8 +6,8 @@ g = Graph.Read_GraphML('Other/fluvial.GraphML')
 lst = aindaPensar(g)
 graphBuilder(g, lst[1], lst[0], 'Fluvial', 'Terrestrial')
 
-lst = aindaPensar(g,3)
-graphBuilder(g, lst[1], lst[0], 'Fluvial-Total-Flow', 'Terrestrial')
+#lst = aindaPensar(g,3)
+#graphBuilder(g, lst[1], lst[0], 'Fluvial-Total-Flow', 'Terrestrial')
 
-lst = aindaPensar(g,2)
-graphBuilder(g, lst[1], lst[0], 'Fluvial-Eff', 'Terrestrial')
+#lst = aindaPensar(g,2)
+#graphBuilder(g, lst[1], lst[0], 'Fluvial-Eff', 'Terrestrial')
